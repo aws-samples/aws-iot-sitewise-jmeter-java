b15b0f5c65135f352b2c89ea5eefcb229b7b13fb
Francisco Gonzalez <franccan@amazon.com>
2023-09-28 18:02:01 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,17 +1,95 @@
-## My Project
 
-TODO: Fill this README out!
+# Welcome to `aws-iot-sitewise-jmeter-java `
 
-Be sure to:
+In this repo you will find two ways of interacting with the AWS IoT SiteWise endpoint that is normally used to ingest data: `BatchPutAssetPropertyValue`.  
 
-* Change the title in this README
-* Edit your repository description on GitHub
+The first one is a Java program that uses standard libraires to build the Sigv4 authentication header and make an HTTP POST request.
 
-## Security
+The second one is a similar program, also written in Java but integrated in the form of a script with JMeter – a commonly used tool to emulate Web traffic.
 
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
 
-## License
+### Step 1 - AWS Credentials
 
-This library is licensed under the MIT-0 License. See the LICENSE file.
+Set your temporary AWS credentials. Make sure your policy allows you to do `iotsitewise:BatchPutAssetPropertyValue`.
 
+```bash
+
+export AWS_ACCESS_KEY_ID=ASIAYLEXAMPLE
+export AWS_SECRET_ACCESS_KEY=KCF0pkigNXke/M7yhEXAMPLE
+export AWS_SESSION_TOKEN=IQoJb3JpZ2luX2VjELD/EXAMPLEHKyOaXM3Of/19ZsiqH8ri86tU=
+
+```
+### Step 2 - Download the project 
+
+```bash
+
+git clone https://github.com/aws-samples/aws-iot-sitewise-jmeter
+
+cd aws-iot-sitewise-jmeter
+
+```
+### Step 3 - Try out sending messages to AWS IoT SiteWise using native Java libraries
+
+``` bash
+
+# Build the project
+mvn clean package
+
+# Set the classpath to include org/apache/httpcomponents/httpclient, commons-logging and the project JAR
+CLASSPATH=target/site-wise-sigv4-test-1.0-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository/org/apache/httpcomponents//httpclient/4.5.13/httpclient-4.5.13.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository/org/apache/httpcomponents//httpcore/4.4.13/httpcore-4.4.13.jar
+CLASSPATH=$CLASSPATH:~/.m2/repository//commons-logging/commons-logging/1.2/commons-logging-1.2.jar
+
+# Inspect "SiteWiseEntries.json" under /extra and run program
+java -cp $CLASSPATH com.amazon.SiteWiseSigV4Test.App extra/SiteWiseEntries.json
+
+#
+# -H "Authorization: AWS4-HM...64660fc47"
+# -H "x-amz-date: 202...110Z"
+# -H "x-amz-security-token: IQoJ...m8A="
+# sendSigV4Post result: {"errorEntries":[]}
+#
+
+```
+
+### Step 4 - Launch JMeter and run demo TestPlan
+
+``` bash
+
+jmeter  site-wise-sigv4-test-jmeter/testplan/SiteWiseEntries.jmx
+
+```
+
+
+
+![jmeter1](site-wise-sigv4-test-jmeter/docs/jmeter1.png)
+<sub><sup>Figure 1. Test plan configured to emulate 10 devices, 1 message every 60 seconds for each one.</sup></sub>
+
+
+![jmeter2](site-wise-sigv4-test-jmeter/docs/jmeter2.png)
+<sub><sup>Figure 2. A sequential deviceId is generate for each device. This is used to build the PropertyAlias for the SiteWise entry.</sup></sub>
+
+![jmeter3](site-wise-sigv4-test-jmeter/docs/jmeter3.png)
+<sub><sup>Figure 3. Template used for the test. Note the 4 placeholders: `entityId` , `deviceId`, `SensorTemperature` and `localTime`</sup></sub>
+
+![jmeter4](site-wise-sigv4-test-jmeter/docs/jmeter4.png)
+<sub><sup>Figure 4. Beanshell script used to create `entryId` and `localTime` values</sup></sub>
+
+![jmeter5](site-wise-sigv4-test-jmeter/docs/jmeter5.png)
+<sub><sup>Figure 5. JMeter config element to generate a random value for `SensorTemperatrue`</sup></sub>
+
+![jmeter6](site-wise-sigv4-test-jmeter/docs/jmeter6.png)
+<sub><sup>Figure 6. Beanshell script that analyses the payload, takes AWS temporary credentials and produces `Authorization` `x-amz-date`. It also passes `AWS_SESSION_TOKEN` as `x-amz-security-token`</sup></sub>
+
+![jmeter7](site-wise-sigv4-test-jmeter/docs/jmeter7.png)
+<sub><sup>Figure 7. The header manager takes variables and sets them as HTTP headers</sup></sub>
+
+
+![jmeter8](site-wise-sigv4-test-jmeter/docs/jmeter8.png)
+<sub><sup>Figure 8. After running the test. You can see the results under "View Results Tree"</sup></sub>
+
+![jmeter9](site-wise-sigv4-test-jmeter/docs/jmeter9.png)
+<sub><sup>Figure 9. You can take a look at the "Response Data" to ensure you are getting the expected result from AWS IoT SiteWise</sup></sub>
+
+Thats it! 
---
+++ site-wise-sigv4-test/src/main/java/com/amazon/SiteWiseSigV4Test/App.java
@@ -0,0 +1,224 @@
+package com.amazon.SiteWiseSigV4Test;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.*;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.io.UnsupportedEncodingException;
+import java.io.File; 
+import java.io.FileNotFoundException;  
+
+import java.util.Scanner;
+import java.net.URL;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import org.apache.http.util.EntityUtils;
+import org.apache.http.entity.StringEntity;
+
+import java.io.IOException;
+
+public class App {
+
+    static String sendSigV4Post(String url, String amazonDate, String amazonAuth, String amazonToken, String payloadJSON) throws IOException {
+
+            String result = "";
+            HttpPost post = new HttpPost(url);
+
+            post.addHeader("content-type", "application/json");
+            post.addHeader("x-amz-date", amazonDate);
+            post.addHeader("Authorization", amazonAuth);
+            post.addHeader("x-amz-security-token", amazonToken);
+            
+            post.setEntity(new StringEntity(payloadJSON));
+
+            try (CloseableHttpClient httpClient = HttpClients.createDefault();
+                CloseableHttpResponse response = httpClient.execute(post)){
+
+                result = EntityUtils.toString(response.getEntity());
+            }
+
+            return result;
+        }
+
+    static String readJSONFile(String fileName){
+        String data = "";
+        try {
+            File fileObject = new File(fileName);
+            Scanner readerObject = new Scanner(fileObject);
+            while (readerObject.hasNextLine()) {
+                data += readerObject.nextLine();
+                if(readerObject.hasNext()) { 
+                    data += "\n";
+                }
+            }
+            readerObject.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Something is wrong");
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    static byte[] getSignatureKey(String key, String dateStamp, String regionName, String serviceName) {
+        try {
+            byte[] kSecret = ("AWS4" + key).getBytes("UTF-8");
+            byte[] kDate = HmacSHA256(dateStamp, kSecret);
+            byte[] kRegion = HmacSHA256(regionName, kDate);
+            byte[] kService = HmacSHA256(serviceName, kRegion);
+            byte[] kSigning = HmacSHA256("aws4_request", kService);
+            return kSigning;
+        } catch (UnsupportedEncodingException  e) {
+             return "ERR".getBytes();
+        }
+    }
+
+    static byte[] HmacSHA256(String data, byte[] key) {
+        try {
+            String algorithm = "HmacSHA256";
+            Mac mac = Mac.getInstance(algorithm);
+            try {
+                mac.init(new SecretKeySpec(key, algorithm));
+                try {
+                    return  mac.doFinal(data.getBytes("UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    System.out.println("Something is wrong");
+                    return "ERR".getBytes();
+                }
+            } catch(InvalidKeyException e) {
+                System.out.println("Something is wrong");
+                return "ERR".getBytes();
+            }
+        } catch(NoSuchAlgorithmException e) {
+            System.out.println("Something is wrong");
+            return "ERR".getBytes();
+            }
+    }
+
+    static String getAmazonDate(LocalDateTime dt) {
+       
+        String amazonDate = String.valueOf(dt.getYear());
+
+        System.out.println("amazonDate:" + amazonDate);
+
+        amazonDate += (dt.getMonthValue() < 10) ? "0" + dt.getMonthValue() : dt.getMonthValue();
+        amazonDate += (dt.getDayOfMonth() < 10) ? "0" + dt.getDayOfMonth() : dt.getDayOfMonth();
+        amazonDate += "T";
+        amazonDate += (dt.getHour() < 10)   ? "0" + dt.getHour()   : dt.getHour();
+        amazonDate += (dt.getMinute() < 10) ? "0" + dt.getMinute() : dt.getMinute();
+        amazonDate += (dt.getSecond() < 10) ? "0" + dt.getSecond() : dt.getSecond();
+        amazonDate += "Z";
+        
+        System.out.println("amazonDate:" + amazonDate);
+        return amazonDate;
+    }
+    final static char[] hexArray = "0123456789abcdef".toCharArray();
+
+    static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+    static byte[] getSha256(String text) {
+        try {
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+            return hash;
+
+        } catch (NoSuchAlgorithmException e) {
+
+            return "ERR".getBytes();
+
+
+        }
+        
+       
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        System.out.println("args: " + args[0]);
+
+        // Make sure we get one parameter
+        String fileName = args[0];
+
+        String AWS_ACCESS_KEY_ID = System.getenv("AWS_ACCESS_KEY_ID");
+        String AWS_SECRET_ACCESS_KEY = System.getenv("AWS_SECRET_ACCESS_KEY");
+        String AWS_SESSION_TOKEN = System.getenv("AWS_SESSION_TOKEN");
+
+        String AWS_REGION = "eu-west-1";
+        String serviceName = "iotsitewise";
+
+        // Get method from sampler
+        // TESTING String method = sampler.getMethod();
+        String method = "POST"; // TESTING
+        System.out.println("Method: " + method);
+
+        // Get body from sampler
+        // TESTING String body = method.equalsIgnoreCase("POST") ? sampler.getArguments().getArgument(0).getValue() : "";
+        String body = readJSONFile(args[0]);
+        System.out.println("body:\n" + body);
+            
+        // Retrieve URL from HTTP Request
+        // TESTING URL url = sampler.getUrl();
+        URL url = new URL("https://data.iotsitewise."+ AWS_REGION +".amazonaws.com/properties"); // TESTING
+
+        // Get host from URL
+        String host = url.getHost(); 
+        System.out.println("Host: " + host);
+        String canonicalUri = url.getPath();
+        System.out.println("Path: " + canonicalUri);
+        
+        String canonicalQuerystring = "";
+        
+        LocalDateTime currentTime = LocalDateTime.now(ZoneOffset.UTC);
+        String amazonDate = getAmazonDate(currentTime);
+        String datestamp = amazonDate.substring(0, 8);
+        String canonicalHeaders = "content-type:application/json\n" 
+                                + "host:" + host + "\n" 
+                                + "x-amz-date:" + amazonDate + "\n" 
+                                + "x-amz-security-token:" + AWS_SESSION_TOKEN + "\n";
+
+        String signedHeaders = "content-type;host;x-amz-date;x-amz-security-token";
+        String payloadHash = bytesToHex(getSha256(body));
+        System.out.println("payloadHash: " + payloadHash);
+
+        String canonicalRequest = method + "\n" + canonicalUri + "\n" + canonicalQuerystring + "\n" + canonicalHeaders + "\n" + signedHeaders + "\n" + payloadHash;
+        System.out.println("Canonical request:\n" + canonicalRequest);
+
+        String algorithm = "AWS4-HMAC-SHA256";
+        String credentialScope = datestamp + "/" + AWS_REGION + "/" + serviceName + "/aws4_request";
+        String stringToSign = algorithm + "\n" + amazonDate + "\n" + credentialScope + "\n" + bytesToHex(getSha256(canonicalRequest));
+        System.out.println("String to sign:\n" + stringToSign);
+
+        byte[] signingKey = getSignatureKey(AWS_SECRET_ACCESS_KEY, datestamp, AWS_REGION, serviceName);
+        String signature = bytesToHex(HmacSHA256(stringToSign, signingKey));
+        String amazonAuth = "AWS4-HMAC-SHA256 Credential=" + AWS_ACCESS_KEY_ID + "/" + credentialScope + ", SignedHeaders=" + signedHeaders + ", Signature=" + signature;
+        System.out.println("###########################################");
+        System.out.println("-H \"Authorization: " + amazonAuth + "\"");
+        System.out.println("-H \"x-amz-date: " + amazonDate + "\"");
+        System.out.println("-H \"x-amz-security-token: " + AWS_SESSION_TOKEN+ "\"");
+
+        try {
+            String result = sendSigV4Post("https://data.iotsitewise."+ AWS_REGION +".amazonaws.com/properties",
+                                    amazonDate, amazonAuth, AWS_SESSION_TOKEN, body );
+            System.out.println("sendSigV4Post result: " + result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
